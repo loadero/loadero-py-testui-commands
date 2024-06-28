78ed09571ce83a82ffffa19234617e62943aaa2a
Andris Papirats-Pfafrats <38584172+andris-p-pfafrats@users.noreply.github.com>
2024-06-28 10:16:13 +0300

    LOAD-3185: Add log message if no alert is open (#11)
    * LOAD-3185: Add log message if no alert is open
    
    * Update setup.py

--- commands/ignore_alert.py
+++ commands/ignore_alert.py
@@ -1,9 +1,13 @@
 from testui.support.testui_driver import TestUIDriver
 from selenium.common.exceptions import NoAlertPresentException
 
+from testui.support import logger
+
 
 def ignore_alert(driver: TestUIDriver) -> None:
     try:
         driver.get_driver().switch_to.alert.accept()
     except NoAlertPresentException:
+        logger.log_debug("[INFO] Loadero: No alert is open.")
+
         pass
